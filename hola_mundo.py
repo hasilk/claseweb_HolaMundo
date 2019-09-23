f8445f9ede02bb81505135206fdd696082d195f2
Francisco <frangoncofre@gmail.com>
2019-09-23 12:27:16 -0300

    Agregamos el hola mundo basico

---
+++ hola_mundo.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#la primeraq linea es para mi linux no es necesaria en windows
+#este es un hola mundo en python
+#Guardar como 'hola_mundo.py' en la carpeta del proyecto git
+
+print('hola mundo')
